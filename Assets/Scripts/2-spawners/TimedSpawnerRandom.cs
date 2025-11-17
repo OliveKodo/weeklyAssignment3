481021aa1a8663854d537ec07895920aaffccb05
Tomer Gal <tomergal@Tomers-MacBook-Pro-2.local>
2025-11-17 12:32:18 +0200

    please

--- Assets/Scripts/2-spawners/TimedSpawnerRandom.cs
+++ Assets/Scripts/2-spawners/TimedSpawnerRandom.cs
@@ -9,9 +9,16 @@ public class TimedSpawnerRandom : MonoBehaviour
 {
     [SerializeField] Mover prefabToSpawn;
     [SerializeField] Vector3 velocityOfSpawnedObject;
-    [Tooltip("Minimum time between consecutive spawns, in seconds")][SerializeField] float minTimeBetweenSpawns = 0.2f;
-    [Tooltip("Maximum time between consecutive spawns, in seconds")][SerializeField] float maxTimeBetweenSpawns = 1.0f;
-    [Tooltip("Maximum distance in X between spawner and spawned objects, in meters")][SerializeField] float maxXDistance = 1.5f;
+
+    [Tooltip("Minimum time between consecutive spawns, in seconds")] [SerializeField]
+    float minTimeBetweenSpawns = 0.2f;
+
+    [Tooltip("Maximum time between consecutive spawns, in seconds")] [SerializeField]
+    float maxTimeBetweenSpawns = 1.0f;
+
+    [Tooltip("Maximum distance in X between spawner and spawned objects, in meters")] [SerializeField]
+    float maxXDistance = 1.5f;
+
     [SerializeField] Transform parentOfAllInstances;
 
     void Start()
@@ -25,11 +32,14 @@ public class TimedSpawnerRandom : MonoBehaviour
         {
             float timeBetweenSpawnsInSeconds = Random.Range(minTimeBetweenSpawns, maxTimeBetweenSpawns);
             await Awaitable.WaitForSecondsAsync(timeBetweenSpawnsInSeconds);       // co-routines
+
             if (!this) break;   // might be destroyed when moving to a new scene
+
             Vector3 positionOfSpawnedObject = new Vector3(
                 transform.position.x + Random.Range(-maxXDistance, +maxXDistance),
                 transform.position.y,
                 transform.position.z);
+
             GameObject newObject = Instantiate(prefabToSpawn.gameObject, positionOfSpawnedObject, Quaternion.identity);
             newObject.GetComponent<Mover>().SetVelocity(velocityOfSpawnedObject);
             newObject.transform.parent = parentOfAllInstances;
--- Assets/Scripts/4-levels/GotoNextLevel.cs
+++ Assets/Scripts/4-levels/GotoNextLevel.cs
@@ -4,7 +4,9 @@ using UnityEngine.SceneManagement;
 public class GotoNextLevel : MonoBehaviour
 {
     [SerializeField] string triggeringTag;
-    [SerializeField][Tooltip("Name of scene to move to when triggering the given tag")] string sceneName;
+
+    [SerializeField] [Tooltip("Name of scene to move to when triggering the given tag")]
+    string sceneName;
     //[SerializeField] NumberField scoreField;
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -13,7 +15,7 @@ public class GotoNextLevel : MonoBehaviour
         {
             Debug.Log("Moving " + this + " to zero");
             this.transform.position = Vector3.zero;
-            SceneManager.LoadScene(sceneName);    // Input can either be a serial number or a name; here we use name.
+            SceneManager.LoadScene(sceneName); // Input can either be a serial number or a name; here we use name.
         }
     }
 }
